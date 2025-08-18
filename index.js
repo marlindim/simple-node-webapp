a645f99ac7786339511533677401b1f12ac57146
marlin <okekeaustinemakuochukwu@gmail.com>
2025-08-18 15:12:39 +0100

    Initial commit: simple Node.js web app

--- index.js
+++ index.js
@@ -1,13 +1,12 @@
-var express = require('express')
-var app = express()
+const express = require("express");
+const app = express();
+const port = process.env.PORT || 3000;
 
-app.set('port', (process.env.PORT || 5000))
-app.use(express.static(__dirname + '/public'))
+app.get("/", (req, res) => {
+  res.send("Hello from Node.js CI/CD pipeline!");
+});
 
-app.get('/', function(request, response) {
-  response.send('Hello World!')
-})
+app.listen(port, () => {
+  console.log(`App running at http://localhost:${port}`);
+});
 
-app.listen(app.get('port'), function() {
-  console.log("Node app is running at localhost:" + app.get('port'))
-})
